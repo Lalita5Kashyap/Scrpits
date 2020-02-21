017f3e981de81ddc75741cb582c4b241440eba6b
Dell <lalitakashyaplk5@gmail.com>
2020-02-21 22:30:26 +0530

    LaunchBrowser.java

--- TestProject/src/Package1/LaunchBrowser.java
+++ TestProject/src/Package1/LaunchBrowser.java
@@ -1,8 +1,17 @@
 package Package1;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class LaunchBrowser {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		String key=("webdriver.gecko.driver");
+		String value=("./exefiles/geckodriver.exe");
+		System.setProperty(key,value);
+		FirefoxDriver driver= new FirefoxDriver();
+		
+		
 
 	}
 
